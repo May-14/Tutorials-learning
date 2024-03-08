1f2acfc07def7a565c88bf5563498e2e89093a40
May <26matmoran@gmail.com>
2024-03-08 14:35:53 -0600

    Add and finish lesson one of odin's js basics course

---
+++ javascript-first-lesson/javascript.js
@@ -0,0 +1,26 @@
+//Task 5
+let MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage);
+
+
+// Task 4
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+// Task 3
+
+console.log((4 + 6 + 9) / 77);
+
+// Task 2
+
+console.log(54 + 23 + 12 + 12 + 32 + 43);
+
+// Task 1
+
+console.log(54 + 23);
+
